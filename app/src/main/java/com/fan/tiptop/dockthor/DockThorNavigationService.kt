b30c0053f8915ec16a5628fbf1a6211e5a59e088
fabriceedon <fabrice.edon@gmail.com>
2022-02-07 21:06:15 -0500

    draft geoloc

---
+++ app/src/main/java/com/fan/tiptop/dockthor/DockThorNavigationService.kt
@@ -0,0 +1,10 @@
+package com.fan.tiptop.dockthor
+
+import android.content.Intent
+import android.os.IBinder
+
+class DockThorNavigationService : android.app.Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
